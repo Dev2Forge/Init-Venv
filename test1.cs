d9d42d1cb1f14d7ef3ffbfd8db9790efaeda5290
tutosrive <tutosrivegamer@gmail.com>
2025-07-18 23:35:54 -0500

    Update/Fix copyright comments

--- test1.cs
+++ test1.cs
@@ -1,30 +1,27 @@
-// Init‑Venv — Initialize a virtual environment automatically.
-// Copyright (C) 2025 Dev2Forge
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/*
+ * Init-Venv - Initialize a virtual environment automatically
+ * Copyright 2025 - 2025 Dev2Forge
+ * Licence: GPL-3
+ * More information: https://github.com/Dev2Forge/Init-Venv/blob/main/LICENSE
+ * Author: tutosrive (tutosrive@Dev2Forge.software)
+ * 
+ * File: \test1.cs
+ * Created: Friday, 18th July 2025 7:00:41 pm
+ * -----
+ * Last Modified: Friday, 18th July 2025 11:29:12 pm
+ * Modified By: tutosrive (tutosrive@Dev2Forge.software)
+ * -----
+ */
+
 
 using InitVenv.src.App.Utils;
-
 namespace InitVenv
 {
     class App1
     {
-
         public static void FileTest(string pathArg)
         {
-            string __fullPath = Paths.ToUniversalPaths(Path.GetFullPath(pathArg));
-            Console.WriteLine(string.Format("{0}", __fullPath));
+            string __fullPath = Paths.ToUniversalPaths(Path.GetFullPath(pathArg)); Console.WriteLine(string.Format("{0}", __fullPath));
         }
     }
 }
