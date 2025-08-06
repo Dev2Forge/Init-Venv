f599231059bc30f76bab3eb30f342482ba96c7a0
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-08-05 19:42:28 -0500

    See #9,  Update the `os.validators` interface.

--- src/App/models/os.validators.cs
+++ src/App/models/os.validators.cs
@@ -8,7 +8,7 @@
  * File: \os.validators.cs
  * Created: Monday, 28th July 2025 3:17:00 pm
  * -----
- * Last Modified: Sunday, 3rd August 2025 8:24:11 pm
+ * Last Modified: Tuesday, 5th August 2025 7:26:14 pm
  * Modified By: tutosrive (tutosrive@Dev2Forge.software)
  * -----
  */
@@ -33,6 +33,12 @@ namespace InitVenv.src.App.models
         /// <param name="workingDir">The path to the working directory</param>
         /// <returns>A bool that represents if <strong>requirements.txt</strong> exists</returns>
         public abstract bool CheckRequirementsFile(string workingDir);
+        /// <summary>
+        /// Check that <strong>dependencies</strong> already install in the <strong>.venv</strong> directory
+        /// </summary>
+        /// <param name="workingDir">The path to the working directory</param>
+        /// <returns>A bool that represents if missing some <strong>dependencies</strong></returns>
+        public abstract Task<bool> CheckRequirementsPip(string workingDir);
         /// <summary>
         /// Validate that the string path of working directory is valid and exists
         /// </summary>
